5d8224b2024c724bf384a5fdae863b40193fdc22
Alasdair Tran <alasdair.tran@gmail.com>
2019-10-26 15:37:03 +1100

    Allow pickle

--- src/align/detect_face.py
+++ src/align/detect_face.py
@@ -83,7 +83,7 @@ class Network(object):
         session: The current TensorFlow session
         ignore_missing: If true, serialized weights for missing layers are ignored.
         """
-        data_dict = np.load(data_path, encoding='latin1').item(
+        data_dict = np.load(data_path, encoding='latin1', allow_pickle=True).item(
         )  # pylint: disable=no-member
 
         for op_name in data_dict:
